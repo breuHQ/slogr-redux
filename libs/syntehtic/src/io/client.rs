91cb02157e1a8a41d7e3422a8b509aa022aa7978
Yousuf Jawwad <yousuf@maher.pk>
2021-11-03 04:21:21 +0500

    chore(synthetic): partial client test with framing

--- libs/syntehtic/src/io/client.rs
+++ libs/syntehtic/src/io/client.rs
@@ -7,7 +7,7 @@ use tokio_stream::StreamExt;
 use tokio_util::codec::{BytesCodec, Decoder};
 use tracing::debug;
 
-use crate::{errors::SyntheticError, io::connection::Connection};
+use crate::{errors::SyntheticError, io::connection::Connection, frames::SynteticFrameCodec};
 
 /// Serves as a container for the client connection.
 #[derive(Debug)]
@@ -21,30 +21,10 @@ impl Client {
   pub async fn connect() -> Result<(), SyntheticError> {
     let addr = "127.0.0.1:9000".parse::<SocketAddr>().unwrap();
     let stream = TcpStream::connect(addr).await?;
-    let mut framed = BytesCodec::new().framed(stream);
+    let mut framed = SynteticFrameCodec::new().framed(stream);
     while let Some(frame) = framed.next().await {
-      match frame {
-        Ok(frame) => {
-          debug!("bytes [{:?}] {:?}", frame.len(), frame);
-          match frame.len() {
-            64 => {
-              let bytes = frame.to_vec();
-              debug!("decoded {:?}", bytes);
-            }
-            164 => {
-              let bytes = frame.to_vec();
-              debug!("decoded {:?}", bytes);
-            }
-            _ => debug!("unknown length"),
-          }
-          // return Ok(());
-        }
-        Err(e) => {
-          return Err(SyntheticError::IOError(e));
-        }
-      }
+      debug!("{:?}", frame);
     }
-    debug!("Do I reach here");
     Err(SyntheticError::IllegalFrame)
   }
 }
